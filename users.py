5da25803f2c197a13a7f2720b7c3bb9030ac750c
Donald Magoua <donald.m@glotelho.com>
2025-07-16 23:57:03 +0200

    Project setup

---
+++ users.py
@@ -0,0 +1,26 @@
+USERS = [    
+    {
+        "id": 1,
+        "company": {
+                "name": "DATUM",
+                "email": "contac@datum-solution.com",
+                "contact": "+237699999999",
+                "location": "Yaounde",
+            },
+        "representative": {
+                "first_name": "John",
+                "last_name": "Dow",
+                "email": "j.dow@datum-solution.com",
+                "contact": "+237677777777",
+                "location": "Yaounde",
+            },
+        "business_categories": [
+            "Wizard",
+            "Adventurer",
+            "Deity"
+        ],
+        "registration_date": "16/07/2027",
+        "subscription_plan": "Datum Solution Starter Plan",
+        "status": 1
+    }
+]
