ef3cf140d684604fc1608aa26f9ff505ec159448
Donald Magoua <donald.m@glotelho.com>
2025-07-18 00:02:48 +0200

    Completed query and params validation

--- app/api/v1/users.py
+++ app/api/v1/users.py
@@ -17,9 +17,8 @@ async def get_all_users():
 async def get_all_users_by_company_name(company_name:str = Query()):
     result = []
     for user in USERS:
-        # if "company" in user and "name" in user["company"]:
-            if company_name.casefold() == user.company.name.casefold():
-                result.append(user)
+        if company_name.casefold() == user.company.name.casefold():
+            result.append(user)
     return result
 
 # GET USERS BY REPRESENTATIVE NAME (AS QUERY PARAM)
@@ -27,34 +26,31 @@ async def get_all_users_by_company_name(company_name:str = Query()):
 async def get_all_users_by_representative_name(representative_name:str = Query()):
     result = []
     for user in USERS:
-        # if "representative" in user and "first_name" in user["representative"] or "last_name" in user["representative"]:
-            if representative_name.casefold() == user.representative.first_name.casefold() or representative_name.casefold() == user.representative.last_name.casefold():
-                result.append(user)
+        if representative_name.casefold() == user.representative.first_name.casefold() or representative_name.casefold() == user.representative.last_name.casefold():
+            result.append(user)
     return result
 
 # GET USERS BY SUBSCRIPTION PLAN (AS QUERY PARAM)
 @router.get("/users/plan")
-async def get_all_users_by_subscription_plan(subscription_plan:int = Query()):
+async def get_all_users_by_subscription_plan(subscription_plan:int = Query(ge=1, le=3)):
     result = []
     for user in USERS:
-        # if "subscription_plan" in user:
         if user.subscription_plan == subscription_plan:
             result.append(user)
     return result
 
 # GET USERS BY STATUS (AS QUERY PARAM)
 @router.get("/users/status")
-async def get_all_users_by_status(status:int = Query()):
+async def get_all_users_by_status(status:int = Query(ge=0, le=1)):
     result = []
     for user in USERS:
-        # if "status" in user:
-            if user.status == status:
-                result.append(user)
+        if user.status == status:
+            result.append(user)
     return result
 
 # GET USERS BY id (AS PATH PARAM)
 @router.get("/user/id/{user_id}")
-async def get_user_by_id(user_id:int = Path()):
+async def get_user_by_id(user_id:int = Path(gt=0)):
     for user in USERS:
         if user.id == user_id:
             return user
@@ -74,7 +70,7 @@ async def update_user(user_body:UserValidation = Body()):
             
 # DELETE USER BY ID AS PATH PARAM
 @router.delete("/user/delete/{user_id}")
-async def delete_user(user_id:int = Path()):
+async def delete_user(user_id:int = Path(gt=0)):
     for i in range(len(USERS)):
         if USERS[i].id == user_id:
             USERS.pop(i)
@@ -82,7 +78,7 @@ async def delete_user(user_id:int = Path()):
         
 # PUT/UPDATE USER STATUS
 @router.put("/user/update-status/{user_id}")
-async def update_user(user_id:int = Path()):
+async def update_user(user_id:int = Path(gt=0)):
     for user in USERS:
         if user.id == user_id:
             current_status = user.status
